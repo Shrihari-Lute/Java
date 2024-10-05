98d5c9b3bd7b214cc76c4dff440f556ec1400acf
Shrihari-Lute <luteshrihari@gmail.com>
2024-10-05 23:43:44 +0530

    Method

--- Employee.java
+++ Employee.java
@@ -1,3 +1,15 @@
+/*Write a Java program that defines a class Employee with the following
+
+Attributes:
+empID (of type int): Represents the employee's ID.
+empName (of type String): Represents the employee's name.
+empSalary (of type float): Represents the employee's salary.
+
+Methods:
+initEmployee(): A method to initialize the employee details with predefined values.
+displayEmployee(): A method to display the employee's details (ID, name, and salary). 
+*/
+
 public class Employee {
     private int empID;
     private String empName;
